9afb9bee48978e76e87f2a4aac711652fdecbab2
Will <howdoicontactwill@gmail.com>
2018-06-02 21:20:24 +0000

    Deploy to GitHub pages [ci skip]

--- js/app.js
+++ js/app.js
@@ -411,7 +411,6 @@ function initClient() {
     }).then(function() {
         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
         updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        $('#buttonLogout').html('Logout ' + gapi.auth2.getAuthInstance().currentUser.Ab.w3.ofa);
         setupUi();
     });
 }
