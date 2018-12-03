330594ad7006004295bc6412bdac531af9e3de78
Will <will.warren@groupbyinc.com>
2018-12-03 00:10:08 +0000

    added utils package + tests

---
+++ js/util.js
@@ -0,0 +1,71 @@
+if (!module) {
+    var module = {};
+    module.exports = {};
+}
+
+var POSITIVE = 1;
+var NEGATIVE = 2;
+
+function parseFilters(filterText) {
+    if (!filterText || filterText.trim() == '') {
+        return [];
+    }
+    var tokens = filterText.trim().split(/\s+/);
+    var filters = [];
+    var phraseToken = '';
+    var phraseTokenType = null;
+    for (var i = 0; i < tokens.length; i++) {
+        var token = tokens[i].trim().toLowerCase();
+        if (phraseTokenType != null || token.startsWith('"') || token.startsWith('-"') || token.endsWith('"')) {
+            if (token.startsWith('"')) {
+                phraseToken += token.substr(1);
+                phraseTokenType = POSITIVE;
+                continue;
+            }
+            else if (token.startsWith('-"')) {
+                phraseToken += token.substring(2);
+                phraseTokenType = NEGATIVE;
+                continue;
+            }
+            else if (token.endsWith('"')) {
+                phraseToken = phraseToken + ' ' + token.substr(0, token.length - 1);
+                token = (phraseTokenType == NEGATIVE ? '-' : '') + phraseToken;
+                phraseToken = '';
+                phraseTokenType = null;
+            }
+            else {
+                phraseToken += ' ' + token;
+                continue;
+            }
+        }
+        if (token.startsWith('-')) {
+            filters.push({ token: token.substring(1), type: NEGATIVE });
+        }
+        else {
+            filters.push({ token: token, type: POSITIVE });
+        }
+    }
+    return filters;
+}
+
+function matchesFilter(filters, haystack) {
+    if (filters.length == 0) {
+        return true;
+    }
+    for (var i = 0; i < filters.length; i++) {
+        if (haystack.includes(filters[i].token) && filters[i].type == NEGATIVE) {
+            return false;
+        }
+    }
+    for (var i = 0; i < filters.length; i++) {
+        if (!haystack.includes(filters[i].token) && filters[i].type == POSITIVE) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports.parseFilters = parseFilters;
+module.exports.NEGATIVE = NEGATIVE;
+module.exports.POSITIVE = POSITIVE;
+module.exports.matchesFilter = matchesFilter;
